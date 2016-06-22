ad3a6f9f4a5f72789936de7f4606ff97f4caace6
Yudi Rahono <palanglung@gmail.com>
2016-06-22 21:19:50 +0700

    FIX: add travis and benchmark

--- freexl_test.go
+++ freexl_test.go
@@ -34,9 +34,27 @@ func TestOpenBinary(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		_, err = OpenBinary(b)
+		w, err := OpenBinary(b)
 		if err != nil {
 			t.Error(err)
 		}
+		t.Logf("MaxRow: %d, MaxCol: %d, Name: %s, Count: %d",
+			w.Sheets[0].MaxRow,
+			w.Sheets[0].MaxCol,
+			w.Sheets[0].Name,
+			len(w.Sheets[0].Values))
+	}
+}
+
+func BenchmarkOpen(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Open("./_xls/simple2003_4WB.xlw")
+	}
+}
+
+func BenchmarkOpenBinary(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		buff, _ := ioutil.ReadFile("./_xls/simple2003_4WB.xlw")
+		OpenBinary(buff)
 	}
 }
